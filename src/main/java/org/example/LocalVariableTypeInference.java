f56fd9097b290c13342db7e9ce3fdbac42f5cc35
Rory Mctague <rory.mctague@merative.com>
2024-05-30 16:17:43 +0100

    Adding a local variable type inference example

---
+++ src/main/java/org/example/LocalVariableTypeInference.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.HashMap;
+
+public class LocalVariableTypeInference {
+
+       // style guide: https://openjdk.org/projects/amber/guides/lvti-style-guide
+    // https://dzone.com/articles/var-work-in-progress
+
+        void now() {
+
+            var myString = "onetwothree";
+            var myMap = new HashMap<String, String>();
+
+
+
+
+        }
+}
